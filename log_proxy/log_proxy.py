464e71fab90960ea66194f248a9bbb3b9686ff3c
Đức Mạnh <122011082+p3dm@users.noreply.github.com>
2026-07-28 12:01:00 +0700

    Refactor log_proxy.py for improved functionality

--- log_proxy/log_proxy.py
+++ log_proxy/log_proxy.py
@@ -2,7 +2,7 @@ import os
 import uiautomator2 as u2
 import time
 from multiprocessing import Process
-from log_unity.unity_login import get_bold_phone_rows
+import subprocess
 
 XPATH = {
     'app': "com.vat.proxyconnector",
@@ -11,66 +11,116 @@ XPATH = {
     'port': '//*[@resource-id="com.vat.proxyconnector:id/edtPortContainer"]',
     'username': '//*[@resource-id="com.vat.proxyconnector:id/edtUsernameContainer"]',
     'password': '//*[@resource-id="com.vat.proxyconnector:id/edtPasswordContainer"]',
-    'connect': '//*[@text="CONNECT"]',
+    'connect': '//*[@text="CONNECT"]|//*[@resource-id="com.vat.proxyconnector:id/btConnect"]',
+    'connect_vn': '//*[@text="KẾT NỐI"]',
+    'connect_rs': '',
+    'socks5': '//*[@text="socks5"]',
+    'https': '//*[@text="https"]',
+    'time-zone': '//androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.Switch[1][@checked="true"]',
+    'menu': '//*[@resource-id="com.android.settings:id/sesl_action_bar_overflow_button"]',
 }
 
-data_list = get_bold_phone_rows("1KLUV3_u3XS0VtdrwcvzgpRBfoud-drdKDfyYB1Y2xjM", "Sheet1")
+Device_ID = [
+]
+
+Proxy = [
+]
+ADB_PATH = r""
 
 def parse_data(data):
     ip, port, username, password = data.split(":")
     return ip, port, username, password
 
+def rotate(device_id):
+    subprocess.run([
+        ADB_PATH, "-s", device_id,
+        "shell", "settings", "put",
+        "system", "accelerometer_rotation",
+        "0"
+    ])
 
-def main(data):
-    proxy = data['proxy']
-    device_id = data['Phone_ID']
+def choose_zone(d):
+    d.xpath('//*[@text="Select time zone"]').click()
+    time.sleep(2)
+    d.xpath(XPATH['menu']).click()
+    time.sleep(2)
+    d.xpath('//*[@text="Select by UTC offset"]').click()
+    time.sleep(2)
+    d.swipe(500, 1500, 500, 500, duration=0.2)
+    time.sleep(1)
+    d.swipe(500, 1500, 500, 500, duration=0.2)
+    time.sleep(1)
+    d.swipe(500, 1500, 500, 500, duration=0.2)
+    time.sleep(2)
+    d.xpath('//*[@text="GMT+08:00"]').click()
+    
+
+def main(device_id, proxy):
     ip, port, username, password = parse_data(proxy)
     d = u2.connect(device_id)
+    d.press("home")
+    d.app_clear("com.genfarmer.uiautomator")
     d.app_start(XPATH['app'])
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
-    time.sleep(6)
+    rotate(device_id)
+    time.sleep(10)
     d.xpath(XPATH['protocol_type']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
-    time.sleep(2)
-    d.xpath('//*[@text="socks5"]').click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+    rotate(device_id)
+    time.sleep(5)
+    d.xpath(XPATH['socks5']).click()
+
     time.sleep(2)
     d.xpath(XPATH['ip_address']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+
     time.sleep(2)
     d.send_keys(ip)
     time.sleep(2)
     d.xpath(XPATH['port']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+    rotate(device_id)
     time.sleep(2)
     d.send_keys(port)
     time.sleep(2)
     d.xpath(XPATH['username']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+    rotate(device_id)
     time.sleep(2)
     d.send_keys(username)
     time.sleep(2)
     d.xpath(XPATH['password']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+    rotate(device_id)
     time.sleep(2)
     d.send_keys(password)
     time.sleep(2)
     d.xpath(XPATH['connect']).click()
-    os.system(f"adb -s {device_id} shell settings put system accelerometer_rotation 0")
+    rotate(device_id)
     time.sleep(5)
     if d.xpath('//*[@text="OK"]').exists:
         d.xpath('//*[@text="OK"]').click()
-    time.sleep(2)
+        time.sleep(2)
     if d.xpath('//*[@text="Allow"]').exists:
         d.xpath('//*[@text="Allow"]').click()
+        time.sleep(2)
+    d.press("home")
+    time.sleep(2)
+    # subprocess.run([
+    #     ADB_PATH, "-s", device_id,
+    #     "shell", "am", "start", "-a","android.settings.DATE_SETTINGS"
+    # ])
+    # rotate(device_id)
+    # time.sleep(5)
+    # if d.xpath(XPATH['time-zone']).exists:
+    #     d.xpath(XPATH['time-zone']).click()
+    #     choose_zone(d)
+    # else:
+    #     choose_zone(d)
+    # time.sleep(2)
+    # d.press("home")
+        
 
 
 if __name__ == "__main__":
     processes = []
-    for data in data_list:
-        p = Process(target=main, args=(data,))
+    for device_id, proxy in zip(Device_ID, Proxy):
+        p = Process(target=main, args=(device_id, proxy))
         processes.append(p)
         p.start()
     for p in processes:
         p.join()
-
